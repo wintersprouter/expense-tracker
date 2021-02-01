436d57e42b0fdb4d59650a098927c520aad39f79
Ashley <bornonefreekiss@alum.ccu.edu.tw>
2021-02-01 22:49:35 +0800

    refactor: modify express routes

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
 const express = require('express')
-const app = express()
 const exphbs = require('express-handlebars')
-const Record = require('./models/Record')
+const routes = require('./routes')
+require('./config/mongoose')
+
+const app = express()
+
 require('./config/mongoose')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main', extname: '.handlebars' }))
 app.set('view engine', 'handlebars')
 
-app.get('/', (req, res) => {
-  Record.find()
-    .lean()
-    .then(records => res.render('index', { records }))
-    .catch(error => console.error(error))
-})
+app.use(routes)
 
 app.listen(3000, () => {
   console.log('App is running on http://localhost:3000')
