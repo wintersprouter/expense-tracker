4fa3a9493d14c53e9944387cbf84813f1c214101
Ashley <bornonefreekiss@alum.ccu.edu.tw>
2021-06-04 21:00:55 +0800

    refactor: refactor userController by async/await

--- controllers/userContorller.js
+++ controllers/userContorller.js
@@ -2,17 +2,21 @@ const User = require('../models/User')
 const bcrypt = require('bcryptjs')
 
 const userController = {
+
   getLoginPage: (req, res) => {
     res.render('login')
   },
+
   userLogin: (req, res) => {
     req.flash('success_msg', '成功登入！')
     res.redirect('/')
   },
+
   getRegisterPage: (req, res) => {
     res.render('register')
   },
-  userRegister: (req, res) => {
+
+  userRegister: async (req, res) => {
     let { name, email, password, confirmPassword } = req.body
     const errors = []
     if (!email || !password || !confirmPassword) {
@@ -23,36 +27,46 @@ const userController = {
       password = ''
       confirmPassword = ''
     }
-    User.findOne({ email }).then(user => {
+    if (errors.length) {
+      return res.render('register', {
+        errors,
+        name,
+        email,
+        password,
+        confirmPassword
+      })
+    }
+
+    try {
+      const user = await User.findOne({ email })
       if (user) {
-        errors.push({ message: '此會員信箱已註冊！' })
+        req.flash('warning_msg', '此會員信箱已註冊！')
         email = ''
-      }
-      if (errors.length) {
         return res.render('register', {
-          errors,
           name,
           email,
           password,
           confirmPassword
         })
       }
-      return bcrypt
-        .genSalt(10)
-        .then(salt => bcrypt.hash(password, salt))
-        .then(hash =>
-          User.create({
-            name,
-            email,
-            password: hash
-          }))
-        .then(user => {
-          req.flash('success_msg', `${email} 成功註冊帳號！`)
-          return res.redirect('/login')
+      const salt = await bcrypt.genSalt(10)
+      const hash = await bcrypt.hash(password, salt)
+      await User.create({
+        name, email, password: hash
+      })
+      req.flash('success_msg', `${email} 成功註冊帳號！`)
+      return res.redirect('/login')
+    } catch (err) {
+      console.log(err)
+      res.render('register', {
+          name,
+          email,
+          password,
+          confirmPassword
         })
-        .catch(error => res.status(404))
-    })
+    }
   },
+  
   userLogout: (req, res) => {
     req.logout()
     req.flash('success_msg', '你已經成功登出。')
