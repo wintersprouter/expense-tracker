ad1cf015d990c2b80555d9992caeb02fbf73ee70
Ashley <bornonefreekiss@alum.ccu.edu.tw>
2021-06-04 20:37:08 +0800

    refactor: refactor homeController by async/await

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -4,80 +4,67 @@ const { getTotalAmount } = require('../public/javascripts/getTotalAmount')
 
 const homeController = {
 
-  getHomePage: (req, res) => {
-    const userId = req.user._id
-    const months = []
-    for (let i = 0; i < 12; i++) {
-      months.push({ month: `${i + 1}` })
+  getHomePage: async (req, res) => {
+    try {
+      const userId = req.user._id
+      const months = []
+      for (let i = 0; i < 12; i++) {
+        months.push({ month: `${i + 1}` })
+      }
+      const [categories, records] = await Promise.all([Category.find().lean().sort({ _id: 'asc' }), Record.find({ userId }).populate('category').lean().sort({ date: 'desc' })])
+      const totalAmountText = getTotalAmount(records)
+      records.forEach(record => {
+        record.date = record.date.toJSON().substr(0, 10)
+      })
+      return res.render('index', { records, totalAmountText, categories, months })
+    } catch (err) {
+      console.log(err)
     }
-    Category.find()
-      .lean()
-      .sort({ _id: 'asc' })
-      .then(categories => {
-        Record.find({ userId })
-          .populate('category')
-          .lean()
-          .sort({ date: 'desc' })
-          .then(records => {
-            const totalAmountText = getTotalAmount(records)
-            records.forEach(record => {
-              record.date = record.date.toJSON().substr(0, 10)
-            })
-            res.render('index', { records, totalAmountText, categories, months })
-          }).catch(error => res.status(404))
-      }).catch(error => res.status(404))
   },
 
-  filterRecords: (req, res) => {
-    const filteredCategory = req.query.category
-    const filteredMonth = Number(req.query.month) - 1
-    const filteredMonthText = req.query.month
-    const filter = {
-      userId: req.user._id
-    }
-
-    const months = []
-    for (let i = 0; i < 12; i++) {
-      months.push({ month: `${i + 1}` })
-    }
-
-    if (filteredCategory !== 'all') {
-      filter.category = filteredCategory
-    }
-
-    if (filteredMonth >= 0) {
-      const today = new Date()
-      const thisYear = today.getUTCFullYear()
-      // month:0-11
-      const startTime = new Date(thisYear, filteredMonth, 1)
-      const endTime = new Date(thisYear, filteredMonth, 31)
-      filter.date = {
-        $gte: startTime,
-        $lte: endTime
+  filterRecords: async (req, res) => {
+    try {
+      const filteredCategory = req.query.category
+      const filteredMonth = Number(req.query.month) - 1
+      const filteredMonthText = req.query.month
+      const filter = {
+        userId: req.user._id
       }
-    }
 
-    Category.find({})
-      .lean()
-      .sort({ _id: 'asc' })
-      .then((categories) => {
-        categories.forEach(category => {
-          category._id = JSON.stringify(category._id).slice(1, -1)
-        })
-        Record.find(filter)
-          .populate('category')
-          .lean()
-          .sort({ date: 'desc' })
-          .then(records => {
-            const totalAmountText = getTotalAmount(records)
-            records.forEach(record => {
-              record.date = record.date.toJSON().substr(0, 10)
-            })
-            return res.render('index', { records, totalAmountText, categories, months, filteredCategory, filteredMonthText })
-          })
-          .catch(error => res.status(404))
+      const months = []
+      for (let i = 0; i < 12; i++) {
+        months.push({ month: `${i + 1}` })
+      }
+
+      if (filteredCategory !== 'all') {
+        filter.category = filteredCategory
+      }
+
+      if (filteredMonth >= 0) {
+        const today = new Date()
+        const thisYear = today.getUTCFullYear()
+        const startTime = new Date(thisYear, filteredMonth, 1)
+        const endTime = new Date(thisYear, filteredMonth, 31)
+        filter.date = {
+          $gte: startTime,
+          $lte: endTime
+        }
+      }
+      const [categories, records] = await Promise.all([
+        Category.find({}).lean().sort({ _id: 'asc' }), Record.find(filter).populate('category').lean().sort({ date: 'desc' })
+      ])
+
+      categories.forEach(category => {
+        category._id = JSON.stringify(category._id).slice(1, -1)
       })
-      .catch(error => res.status(404))
+      const totalAmountText = getTotalAmount(records)
+      records.forEach(record => {
+        record.date = record.date.toJSON().substr(0, 10)
+      })
+      return res.render('index', { records, totalAmountText, categories, months, filteredCategory, filteredMonthText })
+    } catch (err) {
+      console.log(err)
+    }
   }
 }
 
--- controllers/recordController.js
+++ controllers/recordController.js
@@ -5,71 +5,70 @@ const recordController = {
 
   getAddRecordPage: async (req, res) => {
     try {
-    const categories = await Category.find()
-      .lean()
-      .sort({ _id: 'asc' })
-        res.render('new', { categories })
-      } catch (err) {
-    console.log(err)
-  }
+      const categories = await Category.find()
+        .lean()
+        .sort({ _id: 'asc' })
+      res.render('new', { categories })
+    } catch (err) {
+      console.log(err)
+    }
   },
 
   addRecord: async (req, res) => {
     try {
-    const newRecord = req.body
-    if ( newRecord.merchant.length === 0) {
-      newRecord.merchant = '其他'
-    }
-    newRecord.userId = req.user._id
-    
-    const [category,record] = await Promise.all([
-      Category.findOne({ _id: newRecord.category }),Record.create(newRecord)
-    ])
-        req.flash('success_msg', `已成功新增${record.name}的記錄!`)
-        return res.redirect('/')
-        
+      const newRecord = req.body
+      if (newRecord.merchant.length === 0) {
+        newRecord.merchant = '其他'
+      }
+      newRecord.userId = req.user._id
+
+      const [category, record] = await Promise.all([
+        Category.findOne({ _id: newRecord.category }), Record.create(newRecord)
+      ])
+      req.flash('success_msg', `已成功新增${record.name}的記錄!`)
+      return res.redirect('/')
     } catch (err) {
-    console.log(err)
+      console.log(err)
     }
   },
 
   getEditPage: async (req, res) => {
     try {
-    const userId = req.user._id
-    const _id = req.params.id
-    const [record,categories] = await Promise.all([Record.findOne({ _id, userId }).populate('category').lean(),Category.find().lean().sort({ _id: 'asc' })])
-        record.date = record.date.toJSON().substr(0, 10)
-        return res.render('edit', { record, categories })
+      const userId = req.user._id
+      const _id = req.params.id
+      const [record, categories] = await Promise.all([Record.findOne({ _id, userId }).populate('category').lean(), Category.find().lean().sort({ _id: 'asc' })])
+      record.date = record.date.toJSON().substr(0, 10)
+      return res.render('edit', { record, categories })
     } catch (err) {
-    console.log(err)
-  }
+      console.log(err)
+    }
   },
 
   updateRecord: async (req, res) => {
     try {
-    const userId = req.user._id
-    const _id = req.params.id
-    const updateRecord = req.body
+      const userId = req.user._id
+      const _id = req.params.id
+      const updateRecord = req.body
 
-    if (updateRecord.merchant.length === 0) {
-      updateRecord.merchant = '其他'
-    }
-    const [category,record] = await Promise.all([Category.findOne({ _id: updateRecord.category }),Record.findOneAndUpdate({ _id, userId }, updateRecord)])
+      if (updateRecord.merchant.length === 0) {
+        updateRecord.merchant = '其他'
+      }
+      const [category, record] = await Promise.all([Category.findOne({ _id: updateRecord.category }), Record.findOneAndUpdate({ _id, userId }, updateRecord)])
       req.flash('success_msg', '已成功修改此筆記錄!')
       return res.redirect('/')
-      } catch (err) {
-    console.log(err)
+    } catch (err) {
+      console.log(err)
     }
   },
   deleteRecord: async (req, res) => {
     try {
-    const userId = req.user._id
-    const _id = req.params.id
-    const record =await Record.findOneAndDelete({ _id, userId })
-    req.flash('success_msg', ` ${record.name}此筆記錄已成功刪除!`)
-    res.redirect('/')
+      const userId = req.user._id
+      const _id = req.params.id
+      const record = await Record.findOneAndDelete({ _id, userId })
+      req.flash('success_msg', ` ${record.name}此筆記錄已成功刪除!`)
+      res.redirect('/')
     } catch (err) {
-    console.log(err)
+      console.log(err)
     }
   }
 }
